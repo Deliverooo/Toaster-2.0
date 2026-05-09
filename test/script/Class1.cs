6f9a51b1cc12582eabf4bc74ad5aaa06fcfb6b8d
Orbo <ooconnor50@gmail.com>
2026-05-09 16:32:49 +0100

    Basic coreclr work

---
+++ test/script/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Test;
+
+public static class TestClass
+{
+	[UnmanagedCallersOnly]
+	public static int Init(IntPtr p_args, int p_count)
+	{
+		Console.WriteLine("Orbo!");
+
+		return 67;
+	}
+}
